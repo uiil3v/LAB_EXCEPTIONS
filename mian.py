870fb0173bf4c2cc0f4384487211dd91f5d58869
Saif <sknalyami@gmail.com>
2025-06-23 08:36:46 +0300

    I did the lab

---
+++ mian.py
@@ -0,0 +1,17 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+
+
+try:
+    additoin(10, 20)
+
+except NameError:
+    print("Variable not defined")
+else:
+    print("the operation is successful.")
+
+
+
